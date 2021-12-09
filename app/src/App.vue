cc1096f2cdc862ce3de068afcff4859dd23675db
lijiahui-jerry <lijiahui.jerry@qq.com>
2021-12-10 00:14:13 +0800

    day1

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,12 +1,21 @@
 <template >
   <div >
-    我是根组件
+    <Header ></Header >
+    <router-view></router-view>
+    <Footer v-show="$route.meta.show" ></Footer >
   </div >
 </template >
 
 <script >
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
 export default {
   name: '',
+  components: {
+    Header,
+    Footer,
+  },
 }
 </script >
 
---
+++ app/src/pages/Search/index.vue
@@ -0,0 +1,20 @@
+<template >
+  <div >
+    <h1 >params:{{ $route.params }}-------{{ keyword }}----{{ k }}</h1 >
+    <h1 >query:{{ $route.query }}</h1 >
+  </div >
+</template >
+
+<script >
+export default {
+  name: "Search",
+  props: [
+    'keyword',
+    'k',
+  ]
+}
+</script >
+
+<style scoped >
+
+</style >
